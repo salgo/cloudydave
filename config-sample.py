7b0e85203409be2f323fcbea1774f36c2d54a02e
Andy Gale <andy@salgo.net>
2011-10-29 16:54:40 +0100

    Add config example sample config

--- config-sample.py
+++ config-sample.py
@@ -6,3 +6,12 @@ AWS_SDBDomainPrefix = 'cloudydave'
 AWS_SDBRegion = 'eu-west-1'
 
 config = { 'host.com': { 'services': [ 'smtp', 'http', 'https' ] } }
+
+# In our default config here... we're running tests on host.com 
+# The tests of host.com have to fail from two other hosts once before we get notified
+
+# Tests from how many hosts have to fail before we get notified
+NotifyOnHostFailures = 2
+
+# How many failures causes notified
+NotifyOnNumFailues = 1
