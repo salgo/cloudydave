b7b8928d39a58ba27e5aaf62f9c6fd239c02b58d
Andy Gale <andy@salgo.net>
2012-05-02 22:27:25 +0100

    Apply to limit results.

--- results.py
+++ results.py
@@ -6,29 +6,38 @@ from datetime import datetime
 cd = CloudyDave()
 domain = cd.getDomain()
 
-argv.pop(0) # <-- shift
+argv.pop(0) # <-- shift off cmd name
 
 # Fudge together a simple command line query thing.
 
 query = ""
+limit = None
 
-while len(argv) > 2:
+while len(argv) > 1:
     key = argv.pop(0)
     eq = argv.pop(0)
+    
+    if key == 'limit': 
+        limit = int(eq)
+        continue 
+        
     value = argv.pop(0)
     query += ' AND ' + key + ' ' + eq + " '" + value + "'"
 
-#rs = domain.select("SELECT * FROM " + domain.name + " WHERE testhost = 'starbuck.salgo.net' AND timestamp > '0' AND test = 'http' ORDER BY timestamp DESC")
-#for item in rs:
-#    print item
+query = "SELECT * FROM " + domain.name + " WHERE timestamp > '0' " + query + " ORDER BY timestamp DESC"
 
-#rs = domain.select("SELECT * FROM " + domain.name + " where testhost = 'secure.littoralis.com' and test = 'https'");
-rs = domain.select("SELECT * FROM " + domain.name + " WHERE timestamp > '0' " + query + " ORDER BY timestamp DESC")
+print query, "\n"
+
+rs = domain.select(query, max_items=limit)
+c = 0
 for item in rs:
     print "--"
+    c += 1
     for key in item:
         if key == 'timestamp':
             dt = datetime.fromtimestamp(int(item[key]))
             print key, '=', dt.isoformat()
         else:
             print key, '=', item[key]
+
+print "\n", c, "items returned\n"
