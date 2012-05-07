6680c77f9326792c745b55149f594523c724d4a4
Andy Gale <andy@salgo.net>
2012-05-07 20:57:20 +0100

    Very early stages of an error alert notification script. Obviously this needs to store things are in an error state and then notify on failures before we can start using it.
    Also need to move functionality into CloudyDave class.

---
+++ notify.py
@@ -0,0 +1,25 @@
+from CloudyDave import CloudyDave
+from config import config
+from sys import argv
+from datetime import datetime
+from config import notify
+from twilio.rest import TwilioRestClient
+from config import Twilio_AccountSID
+from config import Twilio_AuthToken
+from config import Twilio_SendNumber
+
+cd = CloudyDave()
+domain = cd.getDomain()
+
+for testhost in notify:
+    for test in notify[testhost]:
+        query = "SELECT * FROM {} WHERE timestamp > '0' AND testhost = '{}' AND test = '{}' AND key = '{}' ORDER BY timestamp DESC".format(domain.name, testhost, test['test'], test['key'], test['value'])
+        rs = domain.select(query, max_items=1)
+        
+        for item in rs:                
+            if item['value'] == test['value']:
+                client = TwilioRestClient(Twilio_AccountSID, Twilio_AuthToken)
+
+                message = client.sms.messages.create(to=test['notify-number'],
+                                                     from_=Twilio_SenderNumber,
+                                                     body=testhost + ' ' + item['test'] + ' ' + unicode(item['value']))
